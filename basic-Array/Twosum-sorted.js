0f54b5accc04ee393394a8f187b70241c1aa64eb
amit kumar <amiamit576@gmail.com>
2025-08-20 11:27:22 +0530

    added  few more

---
+++ basic-Array/Twosum-sorted.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const n=nums.length
+    let i=0;
+    let j=n-1;
+    
+    while(i<j){
+        if (nums[i]+nums[j]==target){
+            return[i+1,j+1]
+        }
+         else if(nums[i]+nums[j]>target){
+        j --
+        }
+        else{
+            i++;
+        }
+    }
+    
+       
+    
+};
